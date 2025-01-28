9fac51f263a2a3ccad955c5e38b3066aa69039f9
NICOLAS LIMA MEIRA <24122474@CFP127ALUNO.LOCAL>
2025-01-28 15:58:28 -0300

    js ok

---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict"
+
+const botaoTrocaCor = document.getElementById('troca-cor')
+
+function trocaCor () {
+    const cor = document.getElementById('cor').value
+    document.documentElement.style.setProperty('--cor-fundo', cor)
+}
+
+botaoTrocaCor.addEventListener('click', trocaCor)
